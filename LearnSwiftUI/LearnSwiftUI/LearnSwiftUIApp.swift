5a7b6460f058334bac5a1f0faa91c26e3468eaf8
fqy <18620895340@163.com>
2022-11-18 23:02:09 +0800

    first init

---
+++ LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by 雪飘人间 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
